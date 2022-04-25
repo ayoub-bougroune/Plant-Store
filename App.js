ef226e56cd71132063b9311e6654b5a6ca338817
ayoub-bougroune <ayoubbougroune19@gmail.com>
2022-04-25 16:12:50 +0000

    first commit

--- App.js
+++ App.js
@@ -1,6 +1,25 @@
 import React from 'react';
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import ViewImageScreen from './app/screens/ViewImageScreen'
-export default function App() {
-  return <ViewImageScreen />;
-}
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import HomeScreen from './app/screens/HomeScreen';
+import DetailsScreen from './app/screens/DetailsScreen';
+
+const Stack = createNativeStackNavigator();
+
+import {StatusBar} from 'react-native';
+import COLORS from './app/config/colors';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      {/* <StatusBar barStyle="dark-content" backgroundColor={COLORS.white} /> */}
+      <Stack.Navigator screenOptions={{headerShown: false }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
--- app/config/colors.js
+++ app/config/colors.js
@@ -1,5 +1,9 @@
-export default { 
-    primary: "#fc5c65",
-    secondary: "#4ecdc4",
-    black: "#000"
-}
+const COLORS = {
+    white: '#fff',
+    dark: '#000',
+    red: '#F52A2A',
+    light: '#F1F1F1',
+    green: '#00B761',
+  };
+  
+  export default COLORS;
